4fee5952a5eb414209a887810bac6c029015ce1d
Görkan Gönüldaş <gorkan.gonuldas@felece.com>
2024-05-14 21:41:18 +0300

    word

--- js/plugins-typing.js
+++ js/plugins-typing.js
@@ -78,9 +78,9 @@ document.addEventListener("swup:contentReplaced", function() {
   const text = document.querySelector('.trm-typed-text');
 
   const words = [
-    "UI/UX Designer",
-    "Web Developer",
-    "Photographer",
+    "Computer Engineer",
+    "Java Backend Developer",
+    "SAP Commerce Consultant",
     "Dreamer :)"
   ];
 
